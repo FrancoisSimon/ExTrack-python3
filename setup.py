012fdf77fa38d48bf81f87b3d4465394f4adbb21
FrancoisSimon <87198853+FrancoisSimon@users.noreply.github.com>
2021-07-15 17:45:53 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -77,7 +77,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['numpy', 'itertools', 'lmfit', 'cupy'],
+    install_requires=['numpy', 'lmfit'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
